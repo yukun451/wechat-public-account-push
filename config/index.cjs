a0e8859bc5fc3416a20becb3d454dd8e6541b3a1
yukun451 <56464666+yukun451@users.noreply.github.com>
2023-02-02 15:23:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '梅梅宝贝',
+      name: '梅梅宝贝','yukun',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'olgmS6sJ6LldCuzA1qfqVSxr-8NI',
+      id: 'olgmS6sJ6LldCuzA1qfqVSxr-8NI', 'olgmS6kdB4xjEwlMOWqWopTcNU3Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'P5cB0qa4emqNgcUzlLGLqBRpLW4XbHPgad4aEAbQxf4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD

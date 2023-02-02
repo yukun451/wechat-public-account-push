34a74799737249e4dad5d9c0c88b0a4ce7d2e5e3
yukun451 <56464666+yukun451@users.noreply.github.com>
2023-02-02 14:53:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olgmS6sJ6LldCuzA1qfqVSxr-8NI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7LFP-not_JNInZ2RtRoW491QMfawZaub-qSSiJAjZ_g',
+      useTemplateId: '1v9yLSV3iIttAXWgNEYzSWaAJrJxErSrh4G-h3LM4KQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-23',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'V24o9OwIbdykI2PXTK9kWHoqI9z7O4mpZduEi-nP8rk',
+  CALLBACK_TEMPLATE_ID: 'yitgePmNudV9tzFRd8YMcsAbGzMEME-Gghzmalpx5xM',
 
   CALLBACK_USERS: [
     {

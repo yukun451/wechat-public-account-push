1d1acae662a00ea3b917f226defe229ede8df2f1
yukun451 <56464666+yukun451@users.noreply.github.com>
2023-02-02 15:03:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olgmS6sJ6LldCuzA1qfqVSxr-8NI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1v9yLSV3iIttAXWgNEYzSWaAJrJxErSrh4G-h3LM4KQ',
+      useTemplateId: 'P5cB0qa4emqNgcUzlLGLqBRpLW4XbHPgad4aEAbQxf4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-23',
       festivals: [
